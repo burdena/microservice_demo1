1334b9172de6d6cdbafc46bdecf0c7ff6bc67319
Adam Burden <burden@me.com>
2016-09-23 15:00:28 +0100

    fixed new line

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function (req, res) {
-  res.send('Hello World! - version 1.0.4');
+  res.send('Hello World! - version 1.0.4\n');
   console.log(req.connection.remoteAddress, "/hello");
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
+  console.log('Example app listening on port 3000!');
 });
 
 
