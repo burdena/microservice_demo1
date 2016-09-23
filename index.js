d41e73bc0a5c3c76eeba21192be10ae960f3b835
Adam Burden <burden@me.com>
2016-09-23 14:59:24 +0100

    Edit vers num

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function (req, res) {
-  res.send('Hello World! - version 1.0.3');
+  res.send('Hello World! - version 1.0.4');
   console.log(req.connection.remoteAddress, "/hello");
 });
 
