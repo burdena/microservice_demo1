6ae18f43edf29fd5ca9cd653f22771337f307a52
Adam Burden <burden@me.com>
2016-09-23 15:11:32 +0100

    Added feature
    added the  hostname to the reply

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function (req, res) {
-  res.send('Hello World! - version 1.0.4\n');
+  res.send('Hello World! - version 1.0.4 - ' + req.headers.host);
   console.log(req.connection.remoteAddress, "/hello");
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-
-microservice_demo1/Dockerfile
